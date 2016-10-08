e2e07ec98777c4411fe3f4d140c5e944637c395b
Stefanie Centi <stefcenti@centiumtech.com>
2016-10-08 18:48:51 -0400

    Successful test using the Spoonacular API

--- tests/api-test/FoodApi.js
+++ tests/api-test/FoodApi.js
@@ -1,7 +1,7 @@
 // RecipeFinder is the Main React Component for the Recipe Finder form.
-import axios from 'axios';
+unirest = require('unirest');
 
-/*
+/**** SPOONACULAR API using UNIREST
 // These code snippets use an open-source library. http://unirest.io/nodejs
 unirest.post("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/cuisine")
 .header("X-Mashape-Key", "HpxgpOtGRjmshLUHkFBTPp3BiwZfp1Dcykzjsn12LlQQTUNslw")
@@ -12,26 +12,36 @@ unirest.post("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes
 .end(function (result) {
   console.log(result.status, result.headers, result.body);
 });
-*/
+****/
 
-export default class FoodApi {
+/**** SPOONACULAR API using AXIOS
 
-	constructor(){
+****/
 
-		this.url = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/cuisine';
-		this.headers = {
-			'X-Mashape-Key': 'HpxgpOtGRjmshLUHkFBTPp3BiwZfp1Dcykzjsn12LlQQTUNslw'
-			'Content-Type': 'application/x-www-form-urlencoded'
-			'Accept': 'application/json'
- 		}
+function FoodApi() {
+
+	this.url = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/cuisine';
+
+	this.headers = [
+		{'X-Mashape-Key': 'HpxgpOtGRjmshLUHkFBTPp3BiwZfp1Dcykzjsn12LlQQTUNslw'},
+		{'Content-Type': 'application/x-www-form-urlencoded'},
+		{'Accept': 'application/json'}
+	];
+
+	this.findRecipe = function(ingredientList) {
+
+		// Hardcode ingredient list for now
+	    return unirest.post("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/cuisine")
+			.header("X-Mashape-Key", "HpxgpOtGRjmshLUHkFBTPp3BiwZfp1Dcykzjsn12LlQQTUNslw")
+			.header("Content-Type", "application/x-www-form-urlencoded")
+			.header("Accept", "application/json")
+			.send("ingredientList=3 oz pork shoulder")
+			.send("title=Pork roast with green beans")
+			.end(function (result) {
+			  console.log(result.status, result.headers, result.body);
+			});
 
 	}
-
-	findRecipe(ingredientList) {
-
-	    return axios.get(this.url, {
-	      headers: this.headers;
-	    });
-	}
-	
 }
+
+module.exports = FoodApi;
--- tests/api-test/server.js
+++ tests/api-test/server.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var app = express();
+
+var FoodApi = require('./FoodApi');
+
 app.use('/', express.static(__dirname + '/public'));
+
 app.listen(3000, function() {
 	console.log('Timestamp: ', (Date()).toString());
 	console.log('App running on port 3000!');
+
+	var foodApi = new FoodApi();
+	var recipe = foodApi.findRecipe(["Milk"]);
+
+	console.log(recipe);
 });
