a41f8a6b0f7eeb6ebba771a3ca7b03e704fd810c
Amreeta Choudhury <amreeta89@yahoo.com>
2016-10-11 16:28:49 -0400

    Updated test folder

--- tests/api-test/server.js
+++ tests/api-test/server.js
@@ -1,4 +1,3 @@
-
 var express=require('express');
 var bodyParser = require('body-parser');
 var app = express();
@@ -13,20 +12,39 @@ var FoodApi = require('./FoodApi');
 
 app.use('/', express.static(__dirname + '/public'));
 
+//var _test_case_ = "findRecipe";
+var _test_case_ = "autoCompleteFood";
+
 app.listen(3030, function() {
 	console.log('Timestamp: ', (Date()).toString());
-	console.log('api-test App running on port 3000!');
+	console.log('api-test App running on port 3030!');
 
 	var foodApi = new FoodApi();
-	var result = foodApi.findRecipe("Tomato", "simple", function(response, error){
 
-		if (!error)
-		{
-			console.log("++++ recipes: ", response);
+	if (_test_case_ == "findRecipe") {
+		var result = foodApi.findRecipe("Tomato", "simple", function(response, error){
 
-//			var recipe = response[0];
+			if (!error)
+			{
+				console.log("++++ recipes: ", response);
 
-			console.log("++++ recipe: ", response[0]);
-		}
-	});
+	//			var recipe = response[0];
+
+				console.log("++++ recipe: ", response[0]);
+			}
+		});
+	} else if (_test_case_ == "autoCompleteFood") {
+		var result = foodApi.autoCompleteFood("Apple", "complex", function(response, error){
+
+			if (!error)
+			{
+				console.log("++++ food: ", response);
+
+	//			var recipe = response[0];
+
+	//			console.log("++++ recipe: ", response[0]);
+			}
+		});
+
+	}
 });
