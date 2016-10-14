c1ac08a16b89955906412003a1720a7b413fd78f
Stefanie Centi <stefcenti@centiumtech.com>
2016-10-14 09:57:48 -0400

    created react-test for testing react forms and router

---
+++ tests/react-test/FoodApi.js
@@ -0,0 +1,169 @@
+// RecipeFinder is the Main React Component for the Recipe Finder form.
+unirest = require('unirest');
+
+/**** Some examples of using the SPOONACULAR API with UNIREST npm module
+/*
+/*
+These code snippets use an open-source library. http://unirest.io/nodejs
+TO GET BASIC INFO ABOUT A RECIPE BY INGREDIENTS:
+unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=
+	apples%2Cflour%2Csugar&limitLicense=false&number=5&ranking=1")
+.header("X-Mashape-Key", "HpxgpOtGRjmshLUHkFBTPp3BiwZfp1Dcykzjsn12LlQQTUNslw")
+.header("Accept", "application/json")
+.end(function (result) {
+  console.log(result.status, result.headers, result.body);
+});
+TO PERFORM A COMPLEX SEARCH:
+unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/searchComplex?addRecipeInformation=false&cuisine=american&excludeIngredients=coconut%2C+mango&fillIngredients=false&includeIngredients=onions%2C+lettuce%2C+tomato&intolerances=peanut%2C+shellfish&limitLicense=false&maxCalories=1500&maxCarbs=100&maxFat=100&maxProtein=100&minCalories=150&minCarbs=5&minFat=5&minProtein=5&number=10&offset=0&query=burger&ranking=1&type=main+course")
+.header("X-Mashape-Key", "gYPrulKTKnmshuLI06XFb8coTsw5p1gjhiEjsnMC7d2VWGx88j")
+.header("Accept", "application/json")
+.end(function (result) {
+  console.log(result.status, result.headers, result.body);
+});
+****/
+
+/**** SPOONACULAR API using unirest ****/
+
+/* FoodApi constructor takes the following args 
+	apiKeys: an array of mashape api keys to access spoonacular api
+		simple: used for simple searches from spoonacular 
+		complex: used for complex search from spoonacular
+*/ 
+function FoodApi(apiKeys, limit) {
+
+	if (apiKeys === "") {
+
+		this.apiKeys = {
+			'simple': { apiKey: 'HpxgpOtGRjmshLUHkFBTPp3BiwZfp1Dcykzjsn12LlQQTUNslw' },
+			'complex': { apiKey: 'gYPrulKTKnmshuLI06XFb8coTsw5p1gjhiEjsnMC7d2VWGx88j' },
+		};
+
+	} else {
+
+		this.apiKeys = apiKeys;
+
+	}
+	// By using a complex search, we can get detailed recipe information for 
+	// each recipe with one call to the API.
+	// NOTE: since this saves multiple calls to the API, HOWEVER, for that reason
+	// spoonacular counts this 1 request as 3 requests.
+	// this.searchType = "searchType";
+
+	this.baseUrl = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com';
+
+	this.limit = limit;
+
+	/* findRecipe(ingredientList, searchType, cb)
+		ingredientList: string of ingredients with comma seperator for better results
+		searchType: string = complex/simple for depending on required data
+		cb: call back function returns array of recipes as json obj  */
+
+	this.findRecipe = function(ingredientList, searchType, cb) {
+
+		var self = this;
+
+		var apiKey = self.apiKeys[searchType].apiKey;
+
+		var search = self.baseUrl + (searchType === "complex" ?
+			"/recipes/searchComplex?addRecipeInformation=true&includeIngredients=":
+			"/recipes/findByIngredients?fillIngredients=false&ingredients=");
+
+		var limits = '&limitLicense=false&number=' + self.limit;
+		var url = search + ingredientList + limits + '&ranking=2';
+
+		console.log("**** search string: ", url);
+
+	    return unirest.get(url)
+	    	.header("X-Mashape-Key", apiKey)
+			.header("Accept", "application/json")
+			.end(function (result) {
+				if (result.error) {
+					console.log("**** api key: ", apiKey)
+					console.log("**** error response:\n", result.error)
+					cb(result.error, error = true)
+				} else {
+	  				console.log("**** status: ", result.status);
+	  				console.log("**** headers: ", result.headers);
+	  				console.log("**** body: ", result.body);
+	  				cb((searchType === "complex" ? result.body.results: result.body), error = false);
+	  			}
+  			});
+
+	},
+
+
+	/* autoCompleteFood(searchTerm, count, getDetails, cb)
+		searchTerm: string of ingredients with comma seperator for better results
+		count: number of results
+		getDetails: 
+		cb: call back function returns array of recipes as json obj  */
+
+	this.autoCompleteFood = function(searchTerm, count, getDetails, cb) {
+		// These code snippets use an open-source library.
+		console.log("**** food search term: ", searchTerm);
+
+		var self = this;
+
+		var search = self.baseUrl + "/food/ingredients/autocomplete?";
+
+		var metaInfo = 'metaInformation=' + getDetails;
+		var number = '&number=' + count;
+		var query = '&query=' + searchTerm;
+		var url = search + metaInfo + number + query;
+
+		console.log("**** search string: ", url);
+
+	    return unirest.get(url)
+		.header("X-Mashape-Key", self.apiKeys["complex"].apiKey)
+		.header("Accept", "application/json")
+		.end(function (result) {
+			if (result.error) {
+				console.log("**** error response:\n", result.error)
+				cb(result.error, error = true)
+			} else {
+  				console.log("**** status: ", result.status);
+  				console.log("**** headers: ", result.headers);
+  				console.log("**** body: ", result.body);
+  				cb(result.body, error = false);
+  			}
+		});
+
+	},
+
+	/* autoCompleteRecipe(searchTerm, count, cb)
+		searchTerm: string of recipes with comma seperator for better results
+		count: number of results
+		getDetails: 
+		cb: call back function returns array of recipes as json obj  */
+
+	this.autoCompleteRecipe = function(searchTerm, count, cb) {
+		console.log("**** recipe search term: ", searchTerm);
+
+		var self = this;
+
+		var search = self.baseUrl + "/recipes/autocomplete?";
+		var number = "number=" + count;
+		var query = "&query=" + searchTerm;
+
+		var url = search + number + query;
+
+		console.log("**** search string: ", url);
+
+		unirest.get(url)
+		.header("X-Mashape-Key", self.apiKeys["complex"].apiKey)
+		.header("Accept", "application/json")
+		.end(function (result) {
+			if (result.error) {
+				console.log("**** error response:\n", result.error)
+				cb(result.error, error = true)
+			} else {
+  				console.log("**** status: ", result.status);
+  				console.log("**** headers: ", result.headers);
+  				console.log("**** body: ", result.body);
+  				cb(result.body, error = false);
+  			}
+		});
+	}
+}
+
+module.exports = FoodApi;
---
+++ tests/react-test/server.js
@@ -0,0 +1,120 @@
+// Dependencies
+// =============================================================
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+console.log(__dirname);
+// Sets up the Express app to handle data parsing 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+app.use('/', express.static(__dirname + '/public'));
+
+/**** Hard code a list of recipes for now. ***/
+
+
+(function KeepItSimple(){
+
+// Use the FoodApi to access the Spoonacular Food Api
+
+var f = require('./FoodApi');
+var apiKey = "";
+var foodApi = new f(apiKey, 6);
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get('/recipe', function(req, res){
+	//res.sendFile(path.join(__dirname, '/public/recipe.html'));
+
+	res.json(foodApi.findRecipe());
+
+})
+
+//Added home route
+
+app.get('/', function(req, res){
+	res.sendFile(path.join(__dirname, '/public/landing.html'));
+})
+
+// Added pantry route
+
+app.get('/pantry', function(req, res){
+	res.sendFile(path.join(__dirname, '/public/pantry.html'));
+})
+
+// Added about route
+
+app.get('/about', function(req, res){
+	res.sendFile(path.join(__dirname, '/public/about.html'));
+})
+
+app.get('/signin', function(req, res){
+	res.sendFile(path.join(__dirname, '/public/signin.html'));
+})
+
+// Search for Specific Character (or all characters) - provides JSON
+
+app.get('/recipe/:ingredients?', function(req, res){
+	console.log(1);
+	var term = req.params.ingredients;
+
+	console.log("search term: ", term);
+	console.log("req.params: ", req.params);
+
+	if(term){
+		console.log(2);
+
+		// NOTE: the findRecipe takes a callback function.
+		// This is necessary so it doesn't have to wait before
+		// returning from the unirest call. Otherwise, it would
+		// return too early and we would not have the data yet.
+		var result = foodApi.findRecipe(term, "complex", function(recipes){
+		console.log(3);
+
+			console.log("[GET /api/:ingredients] recipes : ", recipes);
+
+			// Just send one recipe for now, later send entire array
+			res.json(recipes);
+
+		});
+	}
+})
+
+app.get('/ingredient', function(req, res){
+   res.sendFile(path.join(__dirname, 'ingredient.html'));
+})
+
+// Search for a list of matching ingredients following using
+// an autocomplete algorithm to help the user select an item
+// For now, just display the first item in the list.
+app.get('/ingredient/:ingredient?', function(req, res){
+   var chosen = req.params.ingredient;
+   if(chosen){
+       console.log(2);
+
+       var result = foodApi.autoCompleteFood(chosen, 1, true, function(ingredient){
+       console.log(3);
+            // Just send one recipe for now, later send entire array
+
+           res.json(ingredient);
+
+       });
+      
+   }
+});
+
+// app.listen(3000, function() {
+//     console.log('Timestamp: ', (Date()).toString());
+//     console.log('KeepItSimple App running on port 3000!');
+// });
+app.listen(PORT);
+	
+})();
