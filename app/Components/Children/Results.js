2f4b09696f41bb3fd54d629a495eaac3fabcbead
padmajakondeti <padma.kondeti@gmail.com>
2016-10-14 15:15:26 -0400

    updated image size

--- app/Components/Children/Results.js
+++ app/Components/Children/Results.js
@@ -6,7 +6,7 @@ var Result = React.createClass({
       <div className="col-xs-12 col-sm-6 col-md-4 clearfix">
   		  <li className="list-group-item">
             <h4>{this.props.article.title}</h4>
-            <a href={this.props.article.spoonacularSourceUrl} target="_blank"> <img src={this.props.article.image} alt={this.props.article.title} height="100" width="100" /></a>
+            <a href={this.props.article.spoonacularSourceUrl} target="_blank"> <img src={this.props.article.image} alt={this.props.article.title} height="200" width="300" /></a>
   			    <p>Cuisines: {this.props.article.cuisines}</p>
             <p>Ready In Minutes: {this.props.article.readyInMinutes}</p>
             <p>weightWatcherSmartPoints: {this.props.article.weightWatcherSmartPoints} </p>
