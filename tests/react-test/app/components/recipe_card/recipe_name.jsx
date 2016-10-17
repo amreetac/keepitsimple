1a9364f2bb427d784e5aeff463baabfc12c3a748
Stefanie Centi <stefcenti@centiumtech.com>
2016-10-16 23:53:54 -0400

    RecipeCard can now get the data from the RecipeName child object. TODO: modify instructions and ingredients to allow user input then grab the input to be saved in the database on submit

--- tests/react-test/app/components/recipe_card/recipe_name.jsx
+++ tests/react-test/app/components/recipe_card/recipe_name.jsx
@@ -2,14 +2,22 @@
 import React from 'react';
 
 class RecipeName extends React.Component {
-  render() {
-    return(
-      	<div>
-			<label>Name:</label>
-			<input/>
-		</div>
-    )
-  }
+
+	handleChange(e){
+		console.log("RecipeName.handleChange(e): ", e.target.value);
+		console.log("this: ", this);
+		this.props.handleChange(e.target.value);
+	}
+
+	render() {
+		console.log("RecipeName.render()")
+		return(
+		  	<div className = "recipeName">
+				<label>Name:</label>
+				<input type="text" onChange={this.handleChange.bind(this)} />
+			</div>
+		)
+	}
 }
 
 module.exports = RecipeName;
