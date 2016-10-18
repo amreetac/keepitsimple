89e15e6d50aff0359f7c6fd31f3ad073122f6a21
Amreeta Choudhury <amreeta89@yahoo.com>
2016-10-18 17:08:15 -0400

    Fixed about page and styles. Was trying to center the carousel

---
+++ ingredientseed.sql
@@ -0,0 +1,15 @@
+
+use `fab7d2a5n3m9rpt0`;
+
+use ingredient;
+
+INSERT INTO ingredient (foodname, foodimg) VALUES ("baking powder", "https://spoonacular.com/cdn/ingredients_100x100/white-powder.jpg");
+INSERT INTO ingredient (foodname, foodimg) VALUES ("brown sugar", "https://spoonacular.com/cdn/ingredients_100x100/brown-sugar-dark.jpg");
+INSERT INTO ingredient (foodname, foodimg) VALUES ("oil", "https://spoonacular.com/cdn/ingredients_100x100/vegetable-oil.jpg");
+INSERT INTO ingredient (foodname, foodimg) VALUES ("milk", "https://spoonacular.com/cdn/ingredients_100x100/milk.jpg");
+INSERT INTO ingredient (foodname, foodimg) VALUES ("cocoa powder", "https://spoonacular.com/cdn/ingredients_100x100/cocoa-powder.jpg");
+INSERT INTO ingredient (foodname, foodimg) VALUES ("flour", "https://spoonacular.com/cdn/ingredients_100x100/flour.png");
+INSERT INTO ingredient (foodname, foodimg) VALUES ("salt", "https://spoonacular.com/cdn/ingredients_100x100/salt.jpg");
+INSERT INTO ingredient (foodname, foodimg) VALUES ("water", "https://spoonacular.com/cdn/ingredients_100x100/water.jpg");
+INSERT INTO ingredient (foodname, foodimg) VALUES ("sugar", "https://spoonacular.com/cdn/ingredients_100x100/sugar-cubes.jpg");
+INSERT INTO ingredient (foodname, foodimg) VALUES ("onion", "https://spoonacular.com/cdn/ingredients_100x100/brown-onion.jpg");
